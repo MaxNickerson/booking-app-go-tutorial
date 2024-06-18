16edcaf67e83970c7096004e4549fb5d023fccb8
Max Nickerson <41595474+MaxNickerson@users.noreply.github.com>
2024-06-18 13:41:19 -0400

    First commit

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	conferenceName := "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+
+	bookings := []string{}
+
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
+		// asking user for their name
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&firstName)
+
+		fmt.Println("Enter your last name: ")
+		fmt.Scan(&lastName)
+
+		fmt.Println("Enter your email adress: ")
+		fmt.Scan(&email)
+		// fmt.Println(remainingTickets)
+		// fmt.Println(&remainingTickets)
+
+		fmt.Println("Enter number of tickets: ")
+		fmt.Scan(&userTickets)
+
+		remainingTickets = remainingTickets - userTickets
+		// bookings[0] = firstName + " " + lastName
+		bookings = append(bookings, firstName+" "+lastName)
+
+		fmt.Printf("The whole slice: %v\n", bookings)
+		fmt.Printf("The first value: %v\n", bookings[0])
+		fmt.Printf("Slice type: %T\n", bookings)
+		fmt.Printf("Slice length: %v\n", len(bookings))
+
+		fmt.Printf("Thank you %v %v for booking %v tickets. You will recieve a confirmation email at %v\n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+		firstNames := []string{}
+
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+		fmt.Printf("The first names of bookings are %v\n", firstNames)
+	}
+
+}
