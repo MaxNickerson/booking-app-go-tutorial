e1d8cb7c4f5fff945790e0a0bad7983d82054844
Max Nickerson <41595474+MaxNickerson@users.noreply.github.com>
2024-06-22 18:55:24 -0400

    packaged classes

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"booking-app/helper"
 	"fmt"
 	"strings"
 )
@@ -16,7 +17,7 @@ func main() {
 	greetUsers()
 	for {
 		firstName, lastName, email, userTickets := getUserInput()
-		isValidName, isValidEmail, isValidTicketNumber := validateUserInput(firstName, lastName, email, userTickets)
+		isValidName, isValidEmail, isValidTicketNumber := helper.ValidateUserInput(firstName, lastName, email, userTickets, remainingTickets)
 
 		// isValidCity := city == "Singapore" || city == "London"
 
@@ -43,18 +44,6 @@ func main() {
 
 	}
 
-	// city := "London"
-
-	// switch city {
-	// case "New York":
-	// 	// execute code for booking New York conference tickets
-	// case "Singapore":
-	// case "London":
-
-	// default:
-	// 	fmt.Print("No valid city selected")
-
-	// }
 }
 
 func greetUsers() {
@@ -77,13 +66,13 @@ func getFirstNames() []string {
 
 }
 
-func validateUserInput(firstName string, lastName string, email string, userTickets uint) (bool, bool, bool) {
-	isValidName := len(firstName) >= 2 && len(lastName) >= 2
-	isValidEmail := strings.Contains(email, "@")
-	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+// func validateUserInput(firstName string, lastName string, email string, userTickets uint) (bool, bool, bool) {
+// 	isValidName := len(firstName) >= 2 && len(lastName) >= 2
+// 	isValidEmail := strings.Contains(email, "@")
+// 	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
 
-	return isValidName, isValidEmail, isValidTicketNumber
-}
+// 	return isValidName, isValidEmail, isValidTicketNumber
+// }
 
 func getUserInput() (string, string, string, uint) {
 	var firstName string
